b65429fb3f3acf72fd734b7da044c8130febbd9b
Jibran220 <jibranjibran220@gmail.com>
2022-12-16 06:36:03 -0800

    first commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "https://iec-project-server-fr1bv0ytr-jibran220.vercel.app";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
